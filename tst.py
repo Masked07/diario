1595bd96efe258afc2d7c04c03152173823d7877
Masked07 <139254826+Masked07@users.noreply.github.com>
2023-07-11 16:36:09 -0300

    :sparkles: First commit

---
+++ tst.py
@@ -0,0 +1,30 @@
+
+# MAIORIDADE = 18
+# idade = int(input("Digite sua idade: "))
+# if idade >= MAIORIDADE:
+#  print("Maior de idade")
+# else:
+#  print("Menor de idade")
+
+
+a = []
+# b = 1
+# while b < 2:
+#  nome = input("Digite seu nome: ")
+#  idade = int(input("Digite sua idade: "))
+#  nota1 = int(input("Digite sua primeira nota: "))
+#  nota2 = int(input("Digite sua primeira nota: "))
+#  nota3 = int(input("Digite sua primeira nota: "))
+#  print(nome, "/", idade, "/", nota1, "/", nota2, "/", nota3)
+#  b == b + 1
+# media = (nota1+nota2+nota3)/3
+# a.append(media)
+# if a >= 6:
+#   print("Você foi aprovado")
+# else: 
+#   print("Você foi reporvado")
+ 
+ 
+ 
+ 
+ 
